6e66bc0d80459982c0a705f4b3626b832a62b341
mohana22lakshmi-collab <mohana22lakshmi@gmail.com>
2025-12-23 13:14:05 +0530

    Add problem description for division.py
    Added problem description and platform information.

--- division.py
+++ division.py
@@ -1,3 +1,7 @@
+# Problem: Python Division
+# Platform: HackerRank
+# Domain: Python
+# Description: Perform integer division and floating-point division on given numbers.
 if __name__ == '__main__':
     a = int(input())
     b = int(input())
