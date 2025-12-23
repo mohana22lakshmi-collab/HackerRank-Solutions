cac2c20bd9e85452c35d2627bda4920b0ecbb5e2
mohana22lakshmi-collab <mohana22lakshmi@gmail.com>
2025-12-23 13:14:28 +0530

    Add comments for problem statement and description
    Added problem statement and description comments.

--- hello_world.py
+++ hello_world.py
@@ -1,2 +1,6 @@
+# Problem: Say "Hello, World!" With Python
+# Platform: HackerRank
+# Domain: Python
+# Description: Print the string "Hello, World!" to standard output using Python.
 a="Hello, World!"
 print(a)
